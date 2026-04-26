66eab972da884302a259fe352fa1fdcc2703237b
Chang <chang@Changs-MBP.fritz.box>
2026-04-26 17:28:01 +0200

    group validation

--- src/main/java/com/blog/pojo/Category.java
+++ src/main/java/com/blog/pojo/Category.java
@@ -3,12 +3,13 @@ package com.blog.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id;//主键ID
     @NotEmpty
     private String categoryName;//分类名称
@@ -22,5 +23,11 @@ public class Category {
 
     //如果说某个校验项没有指定分组,默认属于Default分组
     //分组之间可以继承, A extends B  那么A中拥有B中所有的校验项
+    public interface Add extends Default {
 
+    }
+
+    public interface Update extends Default{
+
+    }
 }
