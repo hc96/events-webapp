86e89006f1e7d3982af5aefcd7e303e40fc09c16
Chang <chang@Changs-MBP.fritz.box>
2026-04-26 18:02:38 +0200

    article

---
+++ src/main/java/com/blog/service/impl/UserServiceImpl/ArticleServiceImpl.java
@@ -0,0 +1,31 @@
+package com.blog.service.impl.UserServiceImpl;
+
+import com.blog.mapper.ArticleMapper;
+import com.blog.pojo.Article;
+import com.blog.service.ArticleService;
+import com.blog.utils.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleMapper articleMapper;
+
+    @Override
+    public void add(Article article) {
+        //补充属性值
+        article.setCreateTime(LocalDateTime.now());
+        article.setUpdateTime(LocalDateTime.now());
+
+        Map<String,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        article.setCreateUser(userId);
+
+        articleMapper.add(article);
+    }
+}
