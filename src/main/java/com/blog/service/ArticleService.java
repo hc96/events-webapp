ba17bb9e8349b921d46dcd62c33bbe81e57d8674
Chang <chang@Changs-MBP.fritz.box>
2026-04-27 23:30:54 +0200

    pagination on articles

--- src/main/java/com/blog/service/ArticleService.java
+++ src/main/java/com/blog/service/ArticleService.java
@@ -1,8 +1,11 @@
 package com.blog.service;
 
 import com.blog.pojo.Article;
+import com.blog.pojo.PageBean;
 
 public interface ArticleService {
 
     void add(Article article);
+
+    PageBean<Article> list(Integer pageNum, Integer pageSize, Integer categoryId, String state);
 }
--- src/main/java/com/blog/service/impl/UserServiceImpl/ArticleServiceImpl.java
+++ src/main/java/com/blog/service/impl/UserServiceImpl/ArticleServiceImpl.java
@@ -2,12 +2,16 @@ package com.blog.service.impl.UserServiceImpl;
 
 import com.blog.mapper.ArticleMapper;
 import com.blog.pojo.Article;
+import com.blog.pojo.PageBean;
 import com.blog.service.ArticleService;
 import com.blog.utils.ThreadLocalUtil;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -28,4 +32,23 @@ public class ArticleServiceImpl implements ArticleService {
 
         articleMapper.add(article);
     }
+
+
+    @Override
+    public PageBean<Article> list(Integer pageNum, Integer pageSize, Integer categoryId, String state) {
+        PageBean<Article> pb = new PageBean<>();
+
+        PageHelper.startPage(pageNum,pageSize);
+
+        Map<String,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        List<Article> as = articleMapper.list(userId,categoryId,state);
+
+        Page<Article> page = (Page<Article>) as;
+
+        pb.setTotal(page.getTotal());
+        pb.setItems(page.getResult());
+        return pb;
+    }
+
 }
