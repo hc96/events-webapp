38d57b2bb36ebc9856ab7fd39fdca1cd83f30c70
Chang <chang@Changs-MBP.fritz.box>
2026-04-29 22:05:44 +0200

    put token in redis

--- src/main/java/com/blog/interceptors/LoginInterceptor.java
+++ src/main/java/com/blog/interceptors/LoginInterceptor.java
@@ -6,6 +6,9 @@ import com.blog.utils.JwtUtil;
 import com.blog.utils.ThreadLocalUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,11 +17,23 @@ import java.util.Map;
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         String token = request.getHeader("Authorization");
 
         try{
+            // get the token from redis
+            ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+            String redisToken = valueOperations.get(token);
+
+            if(redisToken == null){
+                throw new RuntimeException();
+            }
+
             Map<String, Object> claims = JwtUtil.parseToken(token);
 
             ThreadLocalUtil.set(claims);
